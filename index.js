865ac2d5092f418a768e93e6ec7c6426e9a3c6da
Jose Canahui <jose.canahui@gmail.com>
2019-07-26 11:13:52 -0400

    Append file extension to import

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import SiteData from './SiteData'
+import SiteData from './SiteData.js'
 
 const JSONURL = 'https://spreadsheets.google.com/feeds/list/1nntWrfeSWDfaRAnRjz2CD6VGFnMeKJBYbDqm6i33lr0/od6/public/values?alt=json'
 
