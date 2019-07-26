dae981d0704b54498814cc2dce7314f8d98c6843
Yungchih Chen <yungchih_chen@yahoo.com>
2019-07-26 13:22:15 -0400

    toggle alert box

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import SiteData from './SiteData.js'
 
 const JSONURL = 'https://spreadsheets.google.com/feeds/list/1nntWrfeSWDfaRAnRjz2CD6VGFnMeKJBYbDqm6i33lr0/od6/public/values?alt=json'
 
+var alertBox = document.getElementById("alert");
+
 SiteData(JSONURL).then(data => {
   //enable button
     var btn = document.getElementById("submit-button");
@@ -11,9 +13,12 @@ SiteData(JSONURL).then(data => {
         e.preventDefault();
       var site = data.find(item=>item.code==siteCode.value)
         if (site == undefined){
-          alert('Site code not found!');
+          alertBox.innerHTML = "Site code not found";
+          alertBox.style.display = "block";
+
         } else {
           document.location = site.url;
         }
     });
 })
+
