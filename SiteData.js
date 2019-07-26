8acbf27e86641df75f20d019e96a3f822754292a
Jose Canahui <jose.canahui@gmail.com>
2019-07-26 15:24:30 -0400

    loading state for button

--- SiteData.js
+++ SiteData.js
@@ -1,19 +1,23 @@
+/**
+ * 
+ * @param {String} jsonURL URL of the json representation of the google sheet: 
+ */
+export default async function(jsonURL) {
+  const raw = await fetchSiteData(jsonURL)
+  return mungeSiteData(raw)
+}
+
 async function fetchSiteData(jsonURL) {
-    const res = await fetch(jsonURL)
-    const rawSiteData = await res.json()
-    return rawSiteData.feed.entry
+  const res = await fetch(jsonURL)
+  const rawSiteData = await res.json()
+  return rawSiteData.feed.entry
 }
   
 function mungeSiteData(rawSiteDataEntries) {
-    return rawSiteDataEntries.map(rawSiteDataEntry =>
-        ({
-            code: rawSiteDataEntry['gsx$sitecode']['$t'],
-            url: rawSiteDataEntry['gsx$pre-filledurl']['$t']
-        })
-    ).filter(site => site.code && site.url) //remove empty
-}
-
-export default async function(jsonURL) {
-    const raw = await fetchSiteData(jsonURL)
-    return mungeSiteData(raw)
+  return rawSiteDataEntries.map(rawSiteDataEntry =>
+    ({
+      code: rawSiteDataEntry['gsx$sitecode']['$t'],
+      url: rawSiteDataEntry['gsx$pre-filledurl']['$t']
+    })
+  ).filter(site => site.code && site.url) //remove empty
 }
--- index.js
+++ index.js
@@ -3,12 +3,13 @@ import SiteData from './SiteData.js'
 const JSONURL = 'https://spreadsheets.google.com/feeds/list/1nntWrfeSWDfaRAnRjz2CD6VGFnMeKJBYbDqm6i33lr0/od6/public/values?alt=json'
 
 var alertBox = document.getElementById("alert");
+const btn = document.getElementById("submit-button");
+const loading = document.getElementById("loading");
 
 SiteData(JSONURL).then(data => {
-  //enable button
-  var btn = document.getElementById("submit-button");
   var siteCode = document.getElementById("site-id-input");
-
+  btn.disabled = false;
+  loading.classList.add("hide");
   btn.addEventListener("click", function(e){
     e.preventDefault();
     var site = data.find(item=>item.code==siteCode.value)
